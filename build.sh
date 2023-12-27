16033a6d62c3a9c35f5e37438f6a91223f7adb91
Daniel Miller <daniel@Mac-mini.local>
2023-12-27 12:56:22 +0000

    Rename golang main files to match binaries

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export GOPATH=$(cd .. && pwd) && \
 echo "Setting GOPATH to " $GOPATH && \
 echo "Step 3: Building binaries..." && \
 find search -name "[a-zA-Z]*.go" | xargs go build -o bin/plugin/pogo-plugin-search  &&  \
-go build -o bin/pogod cmd/main.go && \
-go build -o bin/pogo cmd/main_cli.go
+go build -o bin/pogod cmd/main_pogod.go && \
+go build -o bin/pogo cmd/main_pogo.go
