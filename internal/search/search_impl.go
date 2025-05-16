342019424dc00e272ed8b363b7130083cfa4ee6d
Daniel Miller <gate46dmiller@gmail.com>
2025-05-16 09:47:52 +0100

    Add search files

---
+++ internal/search/search_impl.go
@@ -0,0 +1,260 @@
+package search
+
+import (
+	"encoding/json"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+
+	pogoPlugin "github.com/drellem2/pogo/pkg/plugin"
+)
+
+const pogoDir = ".pogo"
+const searchDir = "search"
+
+// API Version for this plugin
+const version = "0.0.1"
+
+const UseWatchers = true
+
+var SearchService = createBasicSearch()
+
+type BasicSearch struct {
+	logger   hclog.Logger
+	projects map[string]IndexedProject
+	watcher  *fsnotify.Watcher
+	updater  *ProjectUpdater
+}
+
+// Input to an "Execute" call should be a serialized SearchRequest
+type SearchRequest struct {
+	// Values: "search" or "files"
+	Type        string `json:"type"`
+	ProjectRoot string `json:"projectRoot"`
+	// Command timeout duration - only for 'search'-type requests
+	Duration string `json:"string"`
+	Data     string `json:"data"`
+}
+
+type SearchResponse struct {
+	Index   IndexedProject `json:"index"`
+	Results SearchResults  `json:"results"`
+	Error   string         `json:"error"`
+}
+
+type ErrorResponse struct {
+	ErrorCode int    `json:"errorCode"`
+	Error     string `json:"error"`
+}
+
+func New() func() (pogoPlugin.IPogoPlugin, error) {
+	return func() (pogoPlugin.IPogoPlugin, error) {
+		return SearchService, nil
+	}
+}
+
+func clean(path string) string {
+	// Append a trailing delimiter if it doesn't exist
+	p := filepath.Clean(path)
+	if p[len(p)-1] != filepath.Separator {
+		p += string(filepath.Separator)
+	}
+	return p
+}
+
+func (g *BasicSearch) printSearchResponse(response SearchResponse) string {
+	// Instead of marshalling the obect, write code to go through all fields
+	// and concatenate them into a string.
+	var str string
+	str += "Index: " + response.Index.Root + "\n"
+	str += "Paths: " + "\n"
+	for _, path := range response.Index.Paths {
+		str += path + "\n"
+	}
+	str += "Results: " + "\n"
+	for _, result := range response.Results.Files {
+		str += "\t" + result.Path + "\n"
+		for _, match := range result.Matches {
+			// Convert match.Content bytes to string
+			var lineStr = strconv.FormatUint(uint64(match.Line), 10)
+			str += "\t\t" + lineStr + "\n"
+			if len(match.Content) > 0 {
+				// str += "\t\t" + string(match.Content) + "\n"
+				str += "\t\t" + match.Content + "\n"
+			} else {
+				str += "\t\t" + "No content" + "\n"
+			}
+		}
+	}
+	str += "Error: " + response.Error + "\n"
+	return str
+}
+
+func (g *BasicSearch) errorResponse(code int, message string) string {
+	resp := ErrorResponse{ErrorCode: code, Error: message}
+	bytes, err := json.Marshal(&resp)
+	if err != nil {
+		g.logger.Error("Error writing error response")
+		panic(err)
+	}
+	return url.QueryEscape(string(bytes))
+}
+
+func (g *BasicSearch) searchResponse(index *IndexedProject, results *SearchResults) string {
+	var response SearchResponse
+	if index == nil {
+		indexedProject := IndexedProject{Root: "", Paths: []string{}}
+		response.Index = indexedProject
+	} else {
+		response.Index = *index
+	}
+	if results == nil {
+		g.logger.Info("Search response was nil")
+		searchResults := SearchResults{}
+		response.Results = searchResults
+	} else {
+		response.Results = *results
+	}
+	response.Error = ""
+
+	bytes, err := json.Marshal(&response)
+	if err != nil {
+		g.logger.Error("Error writing search response")
+		return g.errorResponse(500, "Error writing search response")
+	}
+	return url.QueryEscape(string(bytes))
+}
+
+func (g *BasicSearch) Info() *pogoPlugin.PluginInfoRes {
+	g.logger.Debug("Returning version %s", version)
+	return &pogoPlugin.PluginInfoRes{Version: version}
+}
+
+// Executes a command sent to this plugin.
+func (g *BasicSearch) Execute(encodedReq string) string {
+	g.logger.Debug("Executing request.")
+	req, err2 := url.QueryUnescape(encodedReq)
+	if err2 != nil {
+		g.logger.Error("500 Could not query decode request.", "error", err2)
+		return g.errorResponse(500, "Could not query decode request.")
+	}
+	var searchRequest SearchRequest
+	err := json.Unmarshal([]byte(req), &searchRequest)
+	if err != nil {
+		g.logger.Info("400 Invalid request.", "error", err)
+		return g.errorResponse(400, "Invalid request.")
+	}
+
+	switch reqType := searchRequest.Type; reqType {
+	case "search":
+		searchRequest.ProjectRoot = clean(searchRequest.ProjectRoot)
+		results, err := g.Search(searchRequest.ProjectRoot,
+			searchRequest.Data, searchRequest.Duration)
+		if err != nil {
+			g.logger.Error("500 Error executing search.", "error", err)
+			return g.errorResponse(500, "Error executing search.")
+		}
+		return g.searchResponse(nil, results)
+	case "files":
+		searchRequest.ProjectRoot = clean(searchRequest.ProjectRoot)
+		proj, err3 := g.GetFiles(searchRequest.ProjectRoot)
+		if err3 != nil {
+			g.logger.Error("500 Error retrieving files.", "error", err3)
+			return g.errorResponse(500, "Error retrieving files.")
+		}
+		return g.searchResponse(proj, nil)
+	default:
+		g.logger.Info("404 Unknown request type.", "type", searchRequest.Type)
+		return g.errorResponse(404, "Unknown request type.")
+	}
+
+}
+
+func (g *BasicSearch) ProcessProject(req *pogoPlugin.IProcessProjectReq) error {
+	g.logger.Info("Processing project %s", (*req).Path())
+	proj, err := g.Load((*req).Path())
+	if err != nil {
+		g.logger.Error("Error processing project", "error", err)
+	}
+	if err != nil || len(proj.Paths) == 0 {
+		go g.Index(req)
+	}
+	return nil
+}
+
+func (g *BasicSearch) Close() {
+	g.watcher.Close()
+}
+
+// handshakeConfigs are used to just do a basic handshake betw1een
+// a plugin and host. If the handshake fails, a user friendly error is shown.
+// This prevents users from executing bad plugins or executing a plugin
+// directory. It is a UX feature, not a security feature.
+var handshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  2,
+	MagicCookieKey:   "SEARCH_PLUGIN",
+	MagicCookieValue: "93f6bc9f97c03ed00fa85c904aca15a92752e549",
+}
+
+// Ensure's plugin directory exists in project config
+// Returns full path of search dir
+func (p *IndexedProject) makeSearchDir() (string, error) {
+	fullSearchDir := filepath.Join(p.Root, pogoDir, searchDir)
+	err := os.MkdirAll(fullSearchDir, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	return fullSearchDir, nil
+}
+
+func createBasicSearch() *BasicSearch {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Info,
+		Output:     os.Stderr,
+		JSONFormat: true,
+	})
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		logger.Error("Could not create file watcher. Index will run frequently.")
+	}
+
+	basicSearch := &BasicSearch{
+		logger:   logger,
+		projects: make(map[string]IndexedProject),
+		watcher:  watcher,
+		updater:  nil,
+	}
+	basicSearch.updater = basicSearch.newProjectUpdater()
+
+	if UseWatchers && watcher != nil {
+		go func() {
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok {
+						logger.Warn("Not ok")
+						logger.Warn(event.String())
+						return
+					}
+					if event.Has(fsnotify.Create) || event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
+						logger.Info("File update: ", event)
+						basicSearch.ReIndex(event.Name)
+					}
+				case err, ok := <-watcher.Errors:
+
+					if !ok {
+						return
+					}
+					logger.Error("File watcher error: %v", err)
+				}
+			}
+		}()
+	}
+	return basicSearch
+}
---
+++ internal/search/search_index.go
@@ -0,0 +1,506 @@
+package search
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/sabhiram/go-gitignore"
+	"github.com/sourcegraph/zoekt"
+	"github.com/sourcegraph/zoekt/query"
+
+	pogoPlugin "github.com/drellem2/pogo/pkg/plugin"
+)
+
+const saveFileName = "search_index.json"
+const codeSearchIndexFileName = "code_search_index"
+const indexStartCapacity = 50
+const indexCacheMinutes = 24 * 60
+
+type PogoChunkMatch struct {
+	Line    uint32 `json:"line"`
+	Content string `json:"content"`
+}
+
+type PogoFileMatch struct {
+	Path    string           `json:"path"`
+	Matches []PogoChunkMatch `json:"matches"`
+}
+
+type SearchResults struct {
+	Files []PogoFileMatch `json:"files"`
+}
+
+type IndexedProject struct {
+	Root  string   `json:"root"`
+	Paths []string `json:"paths"`
+}
+
+/*
+*
+
+	Contains channels that can be written to in order to update the project.
+*/
+type ProjectUpdater struct {
+	c        chan *IndexedProject
+	addFw    chan string
+	removeFw chan string
+	quit     chan bool
+	closed   bool
+}
+
+func absolute(path string) (string, error) {
+	str, err := filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+	info, err2 := os.Lstat(path)
+	if err2 != nil {
+		return "", err2
+	}
+	if info.IsDir() {
+		return str + "/", nil
+	}
+	return str, nil
+}
+
+/*
+*
+
+	Returns some channels that can be written to in order to update the project.
+	Starts a goroutine that will read these channels.
+*/
+func (g *BasicSearch) newProjectUpdater() *ProjectUpdater {
+	u := &ProjectUpdater{
+		c:        make(chan *IndexedProject),
+		addFw:    make(chan string),
+		removeFw: make(chan string),
+		quit:     make(chan bool),
+		closed:   false,
+	}
+	go g.write(u)
+	return u
+}
+
+func (g *BasicSearch) write(u *ProjectUpdater) {
+	for !u.closed {
+		func() {
+			select {
+			case proj := <-u.c:
+				g.projects[proj.Root] = *proj
+				g.serializeProjectIndex(proj)
+			case p := <-u.addFw:
+				if g.watcher == nil {
+					g.logger.Warn("watcher is nil")
+				}
+				w := g.watcher.Add(p)
+				if w != nil {
+					g.logger.Error("Error adding file watcher: %v", w)
+				}
+			case p := <-u.removeFw:
+				if g.watcher == nil {
+					g.logger.Warn("watcher is nil")
+				}
+				g.watcher.Remove(p)
+			case <-u.quit:
+				u.closed = true
+			}
+		}()
+	}
+}
+
+// Should only be called by index
+func (g *BasicSearch) indexRec(proj *IndexedProject, path string,
+	gitIgnore *ignore.GitIgnore, u *ProjectUpdater) error {
+	// First index all files in the project
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	dirnames, err := file.Readdirnames(0)
+	g.logger.Debug("Found dirs: ", dirnames)
+	if err != nil {
+		return err
+	}
+	if len(dirnames) == 0 {
+		return nil
+	}
+	files := make([]string, 0, len(dirnames)/2)
+	for _, subFile := range dirnames {
+		newPath := filepath.Join(path, subFile)
+		fileInfo, err := os.Lstat(newPath)
+		if err != nil {
+			g.logger.Warn(err.Error())
+			continue
+		}
+		// Remove projectRoot prefix from newPath
+		relativePath := strings.TrimPrefix(newPath, proj.Root)
+
+		if !gitIgnore.MatchesPath(relativePath) && subFile != ".git" && subFile != ".pogo" {
+			if fileInfo.IsDir() {
+				u.addFw <- newPath
+				err = g.indexRec(proj, newPath, gitIgnore, u)
+				if err != nil {
+					g.logger.Warn(err.Error())
+				}
+			} else {
+				files = append(files, relativePath)
+			}
+		}
+	}
+	proj.Paths = append(proj.Paths, files...)
+	return nil
+}
+
+// Try to index all files in the project, then create a code search index.
+// The first is table stakes - so we error on failure. If the second fails, we log it and return.
+func (g *BasicSearch) index(proj *IndexedProject, path string,
+	gitIgnore *ignore.GitIgnore) {
+
+	u := g.updater
+
+	err := g.indexRec(proj, path, gitIgnore, u)
+	if err != nil {
+		g.logger.Warn("Error indexing project: ", err.Error())
+		return
+	}
+	u.c <- proj
+}
+
+func (g *BasicSearch) ReIndex(path string) {
+	fileInfo, e := os.Lstat(path)
+	if e != nil {
+		g.logger.Error("Error getting path info: ", e)
+		return
+	}
+	if !fileInfo.IsDir() {
+		path = filepath.Dir(path)
+	}
+	g.logger.Info("Reindexing ", path)
+	go func() {
+		fullPath, err2 := absolute(path)
+		if err2 != nil {
+			g.logger.Error("Error getting absolute path", path)
+			return
+		}
+		for projectRoot, indexed := range g.projects {
+			if strings.HasPrefix(fullPath, projectRoot) {
+				/* Below is a golang idiom for removing
+				elements with prefix from the slice. We
+				want to remove all file watchers before
+				reindexing, so we only add back the files
+				that still exist. */
+				relativePath := strings.TrimPrefix(fullPath, projectRoot)
+				paths := indexed.Paths
+				paths2 := paths
+				paths = paths[:0]
+				u := g.updater
+				for _, p := range paths2 {
+					if !strings.HasPrefix(p, relativePath) {
+						paths = append(paths, p)
+					} else {
+						u.removeFw <- p
+					}
+				}
+				indexed.Paths = paths
+
+				gitIgnore, err := ParseGitIgnore(projectRoot)
+				if err != nil {
+					g.logger.Error("Error parsing gitignore %v", err)
+				}
+				g.index(&indexed, fullPath, gitIgnore)
+				break
+			}
+		}
+	}()
+}
+
+/*
+Even if this function encounters an error, it will always at least return a
+GitIgnore that matches nothing.
+*/
+func ParseGitIgnore(path string) (*ignore.GitIgnore, error) {
+	// Read .gitignore if exists
+	ignorePath := filepath.Join(path, ".gitignore")
+	var err error
+	_, err = os.Lstat(ignorePath)
+	var gitIgnore *ignore.GitIgnore
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err = nil
+		}
+		gitIgnore = ignore.CompileIgnoreLines("")
+	} else {
+		gitIgnore, err = ignore.CompileIgnoreFile(ignorePath)
+		if err != nil {
+			gitIgnore = ignore.CompileIgnoreLines("")
+		}
+	}
+	return gitIgnore, err
+}
+
+func (g *BasicSearch) deleteIndexFile(p *IndexedProject) error {
+	searchDir, err := p.makeSearchDir()
+	if err != nil {
+		g.logger.Error("Error making search dir: ", err)
+		return err
+	}
+	indexPath := filepath.Join(searchDir, codeSearchIndexFileName)
+	// First check if indexPath exists
+	_, err = os.Lstat(indexPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		} else {
+			return err
+		}
+	}
+	return os.Remove(indexPath)
+}
+
+func (g *BasicSearch) getSearchFile(p *IndexedProject, filename string) (*os.File, error) {
+	path := p.Root
+	searchDir, err := p.makeSearchDir()
+	if err != nil {
+		g.logger.Error("Error making search dir: ", err)
+		return nil, err
+	}
+	indexPath := filepath.Join(searchDir, filename)
+	indexFile, err := os.OpenFile(indexPath, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		g.logger.Error("Error opening index file ", path)
+		return nil, err
+	}
+	return indexFile, nil
+}
+
+func (g *BasicSearch) getIndexFile(p *IndexedProject) (*os.File, error) {
+	return g.getSearchFile(p, codeSearchIndexFileName)
+}
+
+func (g *BasicSearch) Index(req *pogoPlugin.IProcessProjectReq) {
+	path := (*req).Path()
+	p, ok := g.projects[path]
+	if ok && p.Paths != nil && len(p.Paths) > 0 {
+		g.logger.Info("Already indexed ", path)
+		return
+	}
+	proj := IndexedProject{
+		Root:  path,
+		Paths: make([]string, 0, indexStartCapacity),
+	}
+	gitIgnore, err := ParseGitIgnore(path)
+	if err != nil {
+		// Non-fatal error
+		g.logger.Error("Error parsing gitignore", err)
+	}
+	g.index(&proj, path, gitIgnore)
+}
+
+// Here is the method where we extract the code above
+func (g *BasicSearch) serializeProjectIndex(proj *IndexedProject) {
+	searchDir, err := proj.makeSearchDir()
+	if err != nil {
+		g.logger.Error("Error making search dir: ", err)
+		return
+	}
+	saveFilePath := filepath.Join(searchDir, saveFileName)
+	outBytes, err2 := json.Marshal(proj)
+	if err2 != nil {
+		g.logger.Error("Error serializing index to json", "index", *proj)
+	}
+	err3 := os.WriteFile(saveFilePath, outBytes, 0644)
+	if err3 != nil {
+		g.logger.Error("Error saving index", "save_path", saveFilePath)
+	}
+	g.logger.Info("Indexed " + strconv.Itoa(len(proj.Paths)) + " files for " + proj.Root)
+
+	// Now serialize zoekt index
+
+	// First delete the old index
+	g.deleteIndexFile(proj)
+
+	indexer, err := zoekt.NewIndexBuilder(nil)
+	if err != nil {
+		g.logger.Error("Error creating search index")
+		return
+	}
+
+	// Next create the code search index
+	// TODO - add some useful repository metadata
+	for _, path := range proj.Paths {
+		// Prepend Root to path
+		fullPath := filepath.Join(proj.Root, path)
+		absPath, err := absolute(fullPath)
+		if err != nil {
+			g.logger.Error("Error getting absolute path - file may not exist", path)
+		} else {
+			bytes, err := ioutil.ReadFile(absPath)
+			if err != nil {
+				g.logger.Error("Error reading file ", absPath)
+			} else {
+				indexer.AddFile(absPath, bytes)
+			}
+		}
+	}
+	indexFile, err := g.getIndexFile(proj)
+	if err != nil {
+		g.logger.Error("Error getting index file ", proj.Root)
+		return
+	}
+	defer indexFile.Close()
+	err = indexer.Write(indexFile)
+	if err != nil {
+		g.logger.Error("Error writing index file ", proj.Root)
+		g.logger.Error("Error: ", err.Error())
+		return
+	}
+}
+
+func (g *BasicSearch) Load(projectRoot string) (*IndexedProject, error) {
+	project := &IndexedProject{
+		Root:  projectRoot,
+		Paths: make([]string, 0, indexStartCapacity),
+	}
+	searchDir, err := project.makeSearchDir()
+	if err != nil {
+		g.logger.Error("Error making search dir: ", err)
+		return nil, err
+	}
+	saveFilePath := filepath.Join(searchDir, saveFileName)
+	stat, err := os.Lstat(saveFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			g.projects[projectRoot] = *project
+			// Return empty struct
+			return project, nil
+		}
+		return nil, err
+	}
+	// Check if index is stale
+	if time.Since(stat.ModTime()).Minutes() > indexCacheMinutes {
+		g.logger.Info("Index is stale for " + projectRoot)
+		return project, nil
+	}
+
+	file, err := os.Open(saveFilePath)
+	if err != nil {
+		g.logger.Error("Error opening index file.")
+		return nil, err
+	}
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	err = json.Unmarshal(byteValue, project)
+	if err != nil {
+		g.logger.Error("Error deserializing index file: %v", err)
+		return nil, err
+	}
+	g.logger.Info("Loaded " + strconv.Itoa(len(project.Paths)) + " files for " + projectRoot)
+	g.updater.c <- project
+	return project, nil
+}
+
+func (g *BasicSearch) GetFiles(projectRoot string) (*IndexedProject, error) {
+	project, ok := g.projects[projectRoot]
+	if !ok {
+		return nil, errors.New("Project not indexed " + projectRoot)
+	}
+	return &project, nil
+}
+
+func (g *BasicSearch) Search(projectRoot string, data string, duration string) (*SearchResults, error) {
+	project, ok := g.projects[projectRoot]
+	var knownProjects string
+	for k := range g.projects {
+		knownProjects += k
+	}
+	if !ok {
+		return nil, errors.New("Unknown project " + projectRoot + ". Known projects: " + knownProjects)
+	}
+	// Open index file
+	searchDir, err := project.makeSearchDir()
+	if err != nil {
+		g.logger.Error("Error making search dir: ", err)
+		return nil, err
+	}
+	indexPath := filepath.Join(searchDir, codeSearchIndexFileName)
+	indexFile, err := os.Open(indexPath)
+	if err != nil {
+		g.logger.Error("Error opening index file ", indexPath)
+		return nil, err
+	}
+	defer indexFile.Close()
+	index, err2 := zoekt.NewIndexFile(indexFile)
+	if err2 != nil {
+		g.logger.Error("Error reading index file ", indexPath)
+		return nil, err2
+	}
+	// Search
+	searcher, err := zoekt.NewSearcher(index)
+	if err != nil {
+		g.logger.Error("Error creating searcher", err)
+		return nil, err
+	}
+	defer searcher.Close()
+
+	var (
+		ctx    context.Context
+		cancel context.CancelFunc
+	)
+
+	timeout, err := time.ParseDuration(duration)
+	if err == nil {
+		// The request has a timeout, so create a context that is
+		// canceled automatically when the timeout expires.
+		ctx, cancel = context.WithTimeout(context.Background(), timeout)
+	} else {
+		ctx, cancel = context.WithCancel(context.Background())
+	}
+	defer cancel()
+
+	query, err := query.Parse(data)
+	if err != nil {
+		g.logger.Error("Error parsing query")
+		return nil, err
+	}
+
+	queryOptions := &zoekt.SearchOptions{
+		ChunkMatches: true,
+	}
+
+	result, err := searcher.Search(ctx, query, queryOptions)
+	if err != nil {
+		g.logger.Error("Error searching index")
+		return nil, err
+	}
+
+	// Create PogoFileMatch array of same size as result.Files
+	fileMatches := make([]PogoFileMatch, len(result.Files))
+
+	for i, file := range result.Files {
+		chunkMatches := make([]PogoChunkMatch, len(file.ChunkMatches))
+		for j, match := range file.ChunkMatches {
+			chunkMatches[j] = PogoChunkMatch{
+				Line:    match.ContentStart.LineNumber,
+				Content: "",
+			}
+			if len(match.Content) > 0 {
+				chunkMatches[j].Content = strings.TrimSpace(string(match.Content))
+			}
+		}
+		fileMatches[i] = PogoFileMatch{
+			Path:    strings.Replace(file.FileName, projectRoot, "", 1),
+			Matches: chunkMatches,
+		}
+	}
+	return &SearchResults{
+		Files: fileMatches,
+	}, nil
+}
